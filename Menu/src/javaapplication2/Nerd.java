14eb6bbfb8cd37b5d78279b3494df40511e13c95
m <m.truszczynski@gmail.com>
2014-12-14 11:33:15 +0100

    menu added - the whole project -  later to be marged with the game projects and only code will be stored in gi

---
+++ Menu/src/javaapplication2/Nerd.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author M
+ */
+public class Nerd extends Player{
+    public Nerd(){
+        super();
+        this.setPlayerDescription("Nerd");
+    }
+    
+}
---
+++ Menu/src/javaapplication2/Player.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author M
+ */
+public class Player {
+    private  String playerDescription;
+
+    public void setPlayerDescription(String playerDescription) {
+        this.playerDescription = playerDescription;
+    }
+    public Player(){
+    
+    }
+    private String getPlayerDescription(){
+        return playerDescription;
+       
+    }
+}
