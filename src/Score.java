4a9ae4d59d86bc3d67d9ec95494258b337ce72c9
m <m.truszczynski@gmail.com>
2015-01-14 15:04:57 +0100

    enemy picture added

--- src/Score.java
+++ src/Score.java
@@ -11,6 +11,7 @@ public class Score {
 	private int enemiesKilled = 0;
 	private int itemsCollected = 0;
 	private BufferedImage item;
+	private BufferedImage enemy;
 	private Settings settings;
 	private GamePanel gamePanel;
 	
@@ -23,6 +24,7 @@ public class Score {
 		this.gamePanel = gp;
 		try {
 			item = ImageIO.read(new File("graphics/items/" + this.settings.getItem()));
+			enemy = ImageIO.read(new File("graphics/player/kirbyjump.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -47,15 +49,22 @@ public class Score {
 				item,
 				5,
 				60 ,
-				15,
+				20,
 				30,
 				null
 			);
 		
-		
+		g.drawImage(
+				enemy,
+				5,
+				30 ,
+				20,
+				20,
+				null
+			);
 		g.setColor(Color.GREEN);
-		String tmp = "Enemies killed: " + this.enemiesKilled;
-		g.drawString(tmp, 5, 40);
+		String tmp = " " + this.enemiesKilled;
+		g.drawString(tmp, 25, 50);
 		tmp = "  remaining: " + this.gamePanel.getAmountOfItemLeft();
 		g.drawString(tmp, 20, 80);
 	}
